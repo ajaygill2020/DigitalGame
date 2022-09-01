322f8ea230f77be0d191c0b979c26e71d343e1fb
ajaygill2020 <ajaypalsgill@gmail.com>
2022-09-01 16:56:01 -0400

    Provided detailed information for each juror

--- src/JurorInformation.java
+++ src/JurorInformation.java
@@ -1,18 +1,26 @@
 
 
 public class JurorInformation {
+    int jurNum; // Juror's number ID
+    String jurLastName;  // Juror's last name
+    int jurAge;  // Juror's age
+    boolean jurCollegeDegree;  // Does juror have a college degree? yes = true
 
     // Information about 12 jurors through constructor
-    public JurorInformation(){
-        int jurNum;
-        String jurLastName;
-        short jurAge;
+    public JurorInformation(int jurNum, String jurLastName, int jurAge, boolean jurCollegeDegree){
+        super();
+        this.jurNum = jurNum;
+        this.jurLastName = jurLastName;
+        this.jurAge = jurAge;
+        this.jurCollegeDegree = jurCollegeDegree;
     }
 
-
-
     public void presentJuryInfo() {
+
         System.out.println("Juror number: " + jurNum);
+        System.out.println("Juror last name: " + jurLastName);
+        System.out.println("Juror age: " + jurAge);
+        System.out.println("Juror has college degree? " + jurCollegeDegree);
     }
 
 
--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -8,20 +8,21 @@ public class WelcomeScreen {
         welcomeMessage();
 
     // Juror instance variables
-        JurorInformation jur1 = new JurorInformation();
-        JurorInformation jur2 = new JurorInformation();
-        JurorInformation jur3 = new JurorInformation();
-        JurorInformation jur4 = new JurorInformation();
-        JurorInformation jur5 = new JurorInformation();
-        JurorInformation jur6 = new JurorInformation();
-        JurorInformation jur7 = new JurorInformation();
-        JurorInformation jur8 = new JurorInformation();
-        JurorInformation jur9 = new JurorInformation();
-        JurorInformation jur10 = new JurorInformation();
-        JurorInformation jur11 = new JurorInformation();
-        JurorInformation jur12 = new JurorInformation();
+        JurorInformation jur1 = new JurorInformation(20220100, "Dev", 20,false);
+        JurorInformation jur2 = new JurorInformation(20220200, "Srikkanth", 25,true);
+        JurorInformation jur3 = new JurorInformation(20220300, "Gavaskar", 30,false);
+        JurorInformation jur4 = new JurorInformation(20220400, "Amarnath", 35,true);
+        JurorInformation jur5 = new JurorInformation(20220500, "Sharma", 40,false);
+        JurorInformation jur6 = new JurorInformation(20220600, "Patil", 45,true);
+        JurorInformation jur7 = new JurorInformation(20220700, "Azad", 50,false);
+        JurorInformation jur8 = new JurorInformation(20220800, "Binny", 55,true);
+        JurorInformation jur9 = new JurorInformation(20220900, "Lal", 60,false);
+        JurorInformation jur10 = new JurorInformation(20221000, "Sandhu", 65,true);
+        JurorInformation jur11 = new JurorInformation(20221100, "Shastri", 70,false);
+        JurorInformation jur12 = new JurorInformation(20221200, "Valson", 75,true);
 
     // Juror methods from JurorInformation.java
+        System.out.println("Now it's time to meet the Jury. Remember that in this game, voir-dire does not apply so you will have to work with the following jurors: ");
         jur1.presentJuryInfo();
         jur2.presentJuryInfo();
         jur3.presentJuryInfo();
